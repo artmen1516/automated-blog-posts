9c47c1245e403b78966c1ff3f1decd396dcabd4e
Arturo Mendivil <arturoo1516@gmail.com>
2023-10-11 21:24:35 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import { client } from "@gradio/client";
+import { core } from "@actions/core";
+import { github } from "@actions/github";
+
+console.log(process.env.APP_PROMPT)
+
+const app = await client("yuntian-deng/ChatGPT");
+const result = await app.predict(5, [		
+  process.env.APP_PROMPT, // Prompt
+  1,
+  1,
+  0,
+  [],
+  null,
+]);
+
+console.log(result);
+
+core.setOutput("result", result);
