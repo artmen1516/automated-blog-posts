b9045e5288b07d0cd14cb2033b4e9a89e9cdeca6
Arturo Mendivil <arturoo1516@gmail.com>
2023-10-16 12:59:22 -0700

    changed gradio-client to ws

--- index.js
+++ index.js
@@ -1,48 +1,72 @@
-import { client } from "@gradio/client";
 import * as core from '@actions/core';
 import * as github from '@actions/github'
+import WebSocket from 'ws';
 
 const startTime = new Date()
 console.log('Starting script at ' + startTime.toTimeString())
 console.log('----------------------------------')
 console.log('Input to process:',process.env.APP_PROMPT)
+console.log('----------------------------------')
 
-const app = await client("yuntian-deng/ChatGPT");
+const ws = new WebSocket('wss://yuntian-deng-chatgpt.hf.space/queue/join');
+const randomSession = Math.floor(Math.random() * 1000000000000).toString();
 
-console.log('Processing input...')
-const result = app.submit(5, [		
-  process.env.APP_PROMPT, // Prompt
-  1,
-  1,
-  0,
-  [],
-  null,
-]);
+ws.on('open', () => {
+  console.log('Connected to the WebSocket server');
+  console.log('Processing input...')
+});
 
-let finalResult = "";
+ws.on('message', (data) => {
+  // console.log(`Received: ${data}`);
+  const message = JSON.parse(data);
+  
+  if (message.msg === "send_hash"){
+    const response = {
+      "fn_index":3,
+      "session_hash": randomSession
+    }
+    ws.send(JSON.stringify(response));
+  }
+  if (message.msg === "send_data"){
+    const response = {
+      "fn_index":3,
+      "data":[
+        "como te encuentras el dia de hoy?",
+        1,
+        1,
+        1,
+        [],
+        null
+      ],
+      "event_data":null,
+      "session_hash": randomSession
+    }
+    ws.send(JSON.stringify(response));
+  }
 
-result.on("status", (status) => {
-  if (status.stage === "pending" && status.position != undefined && status.eta != undefined) {
-    console.log('Position in queue:', status.position, ', eta:', status.eta.toFixed(2), 's')
-
-    if (status.position === 0) {
+  if (message.msg === "estimation") {
+    console.log('Position in queue:', message.rank, ', eta:', message.rank_eta.toFixed(2), 's')
+  
+    if (message.rank === 0) {
       console.log("Your turn, generating response...");
-    } 
-  } 
-  else if (status.stage === "complete") {
-    console.log("Chatbot Response:", finalResult.data[0].flat()[1]);
-    core.setOutput("result", finalResult.data[0].flat()[1]);
-
-    const endTime = new Date()
-    console.log('----------------------------------')
-    console.log('Finished at ' + endTime.toTimeString())
-    console.log('Total time elapsed:', (endTime.getTime() - startTime.getTime())/1000, 'seconds')
+    }
   }
 
-  else if (status.stage === "error") {
-    console.log("Chatbot Error:", result.data);
-    core.setOutput("result", "Error");
+  if (message.msg === "process_completed")
+  {
+    console.log("Chatbot Response:", message.output.data[0].flat()[1]);
+    core.setOutput("result", message.output.data[0].flat()[1]);
   }
-}).on("data", (data) => {
-  finalResult = data
+});
+
+ws.on('close', () => {
+  console.log('Connection closed');
+  const endTime = new Date()
+  console.log('----------------------------------')
+  console.log('Finished at ' + endTime.toTimeString())
+  console.log('Total time elapsed:', (endTime.getTime() - startTime.getTime())/1000, 'seconds')
+});
+
+ws.on('error', (error) => {
+  console.error(`WebSocket error: ${error.message}`);
 });
